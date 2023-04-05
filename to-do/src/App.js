9e6eab297ebbe3ca81d08fc82aa9627979c5c671
kamitopalyan <kami.topalyan@hisarschool.k12.tr>
2023-04-05 14:43:32 +0300

    burger

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,38 +1,84 @@
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./App.css";
+import { React, useEffect, useState, Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
 
+  }));
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
+  return result;
+};
 
-function App() {
-  const initialText = '';
-  const [buttonText, setButtonText] = useState(initialText);
-
-  function handleClick() {
-    setButtonText("Hello")
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: ["drag1", "drag2", "drag3"]
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
 
-  return (
+  onDragEnd(result) {   
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  render() {
+    return (
     <div className="App">
-      <button onClick={handleClick}>Add</button>
-        <div class="vertical-container">
-          <row>
-            <div class="item">
-            </div>
-            <div class="item">
-
-            </div>
-            <div class="item">
-
-            </div>
-          </row>
-        </div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+      <Droppable droppableId="droppable-1">
+            {(provided, snapshot) => (
+              <div 
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="vertical-container"
+              >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+                {provided.placeholder}
+              </div>
+            )}  
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
+}
 
 
-
-export default App;
